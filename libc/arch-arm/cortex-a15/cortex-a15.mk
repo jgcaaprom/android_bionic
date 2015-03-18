93b89b872d86d1cdc86d3d5cceb111409a53aa5a
Yingshiuan Pan <yingshiuan.pan@linaro.org>
2015-03-18 10:29:03 +0800

    libc: arm: add optimized memchr implementation
    This optimization is extracted from cortex-strings and bionic-ized,
    then applied to all arm-v7a cpu (a9, a15, denver, krait)
    
    Change-Id: I82f43c8a4a1993f5d063226c4b4ea5866148bb99
    Signed-off-by: Yingshiuan Pan <yingshiuan.pan@linaro.org>
    
    Conflicts:
    	libc/arch-arm/arm.mk
    	libc/arch-arm/cortex-a15/cortex-a15.mk
    	libc/arch-arm/cortex-a9/cortex-a9.mk
    	libc/arch-arm/generic/generic.mk
    	libc/arch-arm/krait/krait.mk

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,7 +1,8 @@
 libc_bionic_src_files_arm += \
-    arch-arm/cortex-a15/bionic/memchr.S \
     bionic/__memcpy_chk.cpp \
     arch-arm/bionic/memcpy.neon.S \
+    arch-arm/bionic/memchr.v7a.S \
+    arch-arm/cortex-a15/bionic/memcpy.S \
     arch-arm/cortex-a15/bionic/memset.S \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
