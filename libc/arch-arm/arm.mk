6fefcfe1188b5e071987b7a4dcd0e36fc6fcfa1f
Kyle Repinski <repinski23@gmail.com>
2015-05-02 00:53:16 -0500

    libc: arm: Optimized strrchr courtesy of NetBSD.
    Change-Id: I295b371522336dd337a881ed0ca50f7f7f295582

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -7,7 +7,6 @@
 libc_bionic_src_files_arm += \
     bionic/memrchr.c \
     bionic/strnlen.c \
-    bionic/strrchr.cpp \
 
 libc_freebsd_src_files_arm += \
     upstream-freebsd/lib/libc/string/wcscat.c \
@@ -42,6 +41,7 @@ libc_bionic_src_files_arm += \
     arch-arm/bionic/setjmp.S \
     arch-arm/bionic/syscall.S \
     arch-arm/bionic/strchr.S \
+    arch-arm/bionic/strrchr.S \
 
 libc_arch_static_src_files_arm := arch-arm/bionic/exidx_static.c
 libc_arch_dynamic_src_files_arm := arch-arm/bionic/exidx_dynamic.c
